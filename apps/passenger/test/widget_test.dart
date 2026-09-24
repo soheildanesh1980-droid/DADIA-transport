94050263344ce1e27e16295c58cc65fe7c70a7d6
Soheil Danesh <soheildanesh1980@gmail.com>
2026-09-25 02:10:01 +0330

    test(passenger): stabilize language in widget test

--- apps/passenger/test/widget_test.dart
+++ apps/passenger/test/widget_test.dart
@@ -1,9 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:dadia_passenger/main.dart';
+import 'package:dadia_passenger/core/language/app_localization.dart';
+
 
 void main() {
   testWidgets('DADIA Passenger login and registration are available',
       (WidgetTester tester) async {
+    languageController.setLanguage('FA');
     await tester.pumpWidget(const DadiaPassengerApp());
 
     expect(find.text('ورود به دادیا'), findsOneWidget);
